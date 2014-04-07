00b083ad0247c8241568c00861e5897d14b7870c
Iain Dawson <iain@colons.co>
2014-04-07 21:34:56 +0100

    Run 50% more collision checks.

--- game.js
+++ game.js
@@ -1491,7 +1491,7 @@ function Game() {
 
       // We don't want to run unnecessary collision checks, so we decide how
       // many to run based on the velocities and radii of the objects involved.
-      for (var progress = 0; progress < 1; progress += (Math.min(enemy.radius, mass.radius))/(2 * Math.max(
+      for (var progress = 0; progress < 1; progress += (Math.min(enemy.radius, mass.radius))/(3 * Math.max(
         enemyPositionDelta.x, enemyPositionDelta.y, massPositionDelta.x, massPositionDelta.y, 1
       ))) {
         enemyPosition = {
