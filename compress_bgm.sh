0d482254a2ba38f223af503f7a609183efaf3dac
Iain Dawson <iain@bldm.us>
2014-12-20 16:30:35 +0000

    Don't delete the bgm until we have a replacement ready.

--- compress_bgm.sh
+++ compress_bgm.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash -e
 
-rm "$1".mp3 || :
-
 cp "$1".aif "$1"_copy1.aif 
 cp "$1".aif "$1"_copy2.aif 
 
 lame -V 0 --nogap "$1"_copy1.aif "$1".aif "$1"_copy2.aif
+rm "$1".mp3 || :
 mv "$1".aif.mp3 "$1".mp3
 
 rm "$1"_copy*
